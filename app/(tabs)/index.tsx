5fdc060ae72bc6c75d72988afbbbda9085173b89
Suraj Kumar <surajgautam56878@gmail.com>
2025-12-26 23:40:34 +0530

    add:code strcture and useTheme fo dark or light mode using asyncStorage and context

---
+++ app/(tabs)/index.tsx
@@ -0,0 +1,20 @@
+import useTheme from "@/hooks/useTheme";
+import { Text, TouchableOpacity, View } from "react-native";
+
+export default function Index() {
+  const { toggleDarkMode } = useTheme();
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <TouchableOpacity onPress={toggleDarkMode}>
+        <Text>Toggle the mode</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,14 @@
+import { ThemeProvider } from "@/hooks/useTheme";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+    <ThemeProvider >
+      <Stack screenOptions={{
+        headerShown: false
+      }}>
+        <Stack.Screen name="(tabs)" />
+      </Stack>
+    </ThemeProvider>
+  )
 }
